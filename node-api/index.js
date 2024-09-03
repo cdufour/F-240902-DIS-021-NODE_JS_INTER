419f111aa286bc9edad23cffe698d006b76aab38
cdufour <opusidea@gmail.com>
2024-09-03 16:05:26 +0200

    exo 4

--- node-api/index.js
+++ node-api/index.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const app = express();
 const PORT = 3200;
+const knex = require('knex')({
+    client: 'sqlite3',
+    connection: {
+        filename: './data/chinook.db'
+    }
+});
 
 app.get('/', (req, res) => {
     res.send('Accueil');
 })
+
+// Ressource: artist
+// requêtes en lecture
+app.get('/artist', (req, res) => {
+    // SELECT Name FROM artists
+    knex.select('Name').from('artists')
+        .then(artists => res.json(artists))
+})
+
+app.get('/artist/:id', (req, res) => {
+    knex
+        .select('*')
+        .from('artists')
+        .where('ArtistId', req.params.id)
+        .then(result => {
+            if (result.length === 0) res.send('Aucun artiste correspondant');
+            res.json(result[0]);
+        })
+})
+
+// requêtes en écriture
+// ToDO...
 
 app.listen(PORT, () => {
     console.log('Server running on ' + PORT + '...')
